ff28a491705821a0e67f467a07084c238b422c55
shimarrudz <vic.shima.vss@gmail.com>
2023-10-05 13:01:46 -0300

    feat: add day 05 challenge using javascript

---
+++ Day05/each-element-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+var map = function(arr, fn) {
+    var mappedArray = [];
+    for(var i = 0; i < arr.length; i++) {
+        mappedArray.push(fn(arr[i], i));
+    }
+    return mappedArray;
+};
