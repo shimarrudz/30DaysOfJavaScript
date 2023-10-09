7d76daac3dbaaa7c101277019c41735d25ed837f
shimarrudz <vic.shima.vss@gmail.com>
2023-10-09 11:31:58 -0300

    feat: add day 07 filter elements js

---
+++ Day07/filter-elements.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn 
+ * @param {number[]}
+ */ 
+
+var filter = function(arr, fn) {
+    var filteredArr = [];
+    for(var i = 0; i < arr.length; i++) {
+        if(fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+}
